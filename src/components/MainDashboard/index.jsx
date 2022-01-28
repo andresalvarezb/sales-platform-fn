9c0f78cb609cd69f0f24739a1afd18381747b4c7
Andrés Álvarez <andres.alvarez094@hotmail.es>
2022-01-28 18:10:16 -0500

    modify the MainDashborad's styles

--- src/components/MainDashboard/index.jsx
+++ src/components/MainDashboard/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Input from '../Input';
 // import IconTask from '../IconTask';
 import './main.css';
 
@@ -10,22 +9,22 @@ import './main.css';
 // ]
 function MainDashboard() {
     return (
-        <div className='container-main'>
-            <main className='main-dashboard'>
-                <form action="" className='form'>
-                    <header className='container-header'>
+        <div className='WrapperMain'>
+            <main className='WrapperMain-dashboard'>
+                <form action="" className='FormDashboard'>
+                    <header className='FormDashboard-header u-p-0'>
                         <h2>New bill</h2>
-                        <div className='header-form'>
-                            <fieldset className='container-customer'>
-                                <label htmlFor="">
+                        <div className='WrapperForm'>
+                            <fieldset className='WrapperForm-fieldset WrapperCustomer'>
+                                <label className='WrapperCustomer-label'>
                                     <p>Type</p>
                                     <select name="electronicBill" id="">
                                         <option value="">---</option>
                                         <option value="EB">Electronic Bill</option>
                                     </select>
                                 </label>
-                                <label>
-                                    <p>name</p>
+                                <label className='WrapperCustomer-label'>
+                                    <p>Name</p>
                                     <input list='list' name='name' />
                                     <datalist id='list'>
                                         <option value="andres">andres</option>
@@ -34,13 +33,19 @@ function MainDashboard() {
                                         <option value="Carlos">Carlos</option>
                                     </datalist>
                                 </label>
-                                <Input type="text" name="Contact" value={'other thing'} disabled/>
-                                <Input type="datetime-local" name="date" />
+                                <label className='WrapperCustomer-label'>
+                                    <p>Contact</p>
+                                    <input type="text" name="Contact" value={'other thing'} disabled/>
+                                </label>
+                                
                             </fieldset>
 
-                            <fieldset >
-                                <Input type="number" name="Number" />
-                                <label htmlFor="">
+                            <fieldset className='WrapperForm-fieldset WrapperForm-fieldset--seller WrapperSeller'>
+                                <label className='WrapperCustomer-label'>
+                                    <p>Date</p>
+                                    <input type="number" name="number"/>
+                                </label>
+                                <label className='WrapperCustomer-label'>
                                     <p>seller</p>
                                     <input list='seller' name="" id="" />
                                     <datalist id='seller'>
@@ -49,6 +54,10 @@ function MainDashboard() {
                                         <option value="Laura">Laura</option>
                                         <option value="Carlos">Carlos</option>
                                     </datalist>
+                                </label>
+                                <label className='WrapperCustomer-label'>
+                                    <p>Date</p>
+                                    <input type="datetime-local" name="date"/>
                                 </label>
                             </fieldset>
                         </div>
